60d0ba4372c32a9ea8794c33404f47613936ee99
Fricatel1987 <Fricatel1987@users.noreply.github.com>
2020-07-12 13:52:25 -0400

    added Square and Rectangle

--- sandbox/src/main/java/ru/stqa/testing/sandox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/testing/sandox/MyFirstProgram.java
@@ -3,7 +3,33 @@ package ru.stqa.testing.sandox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Artur");
+
+
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата sо sтороной " + s.l + " = " + area(s));
+
+    Rectangle r = new Rectangle(4,6);
+       System.out.println("Площадь прaмоугольника sо sторонами " + r.a + " и " + r.b + " = " + area(r));
+
+  }
+  public static void hello(String somebody){
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area (Square s){
+    return s.l * s.l;
+  }
+
+  public static double area (Rectangle r) {
+    return r.a * r.b;
+  }
+
+  public static double area (double l) {
+    return l * l;
   }
 
 }
+
---
+++ sandbox/src/main/java/ru/stqa/testing/sandox/Square.java
@@ -0,0 +1,11 @@
+package ru.stqa.testing.sandox;
+
+public class Square {
+
+    public double l;
+
+    public Square(double l){
+      this.l = l;
+  }
+
+}
