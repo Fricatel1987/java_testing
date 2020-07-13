0702c38da5655d976332f1b92dba9ddb5afd47d1
Fricatel1987 <Fricatel1987@users.noreply.github.com>
2020-07-13 16:00:33 -0400

    methods and objects

--- sandbox/src/main/java/ru/stqa/testing/sandox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/testing/sandox/MyFirstProgram.java
@@ -9,23 +9,17 @@ public class MyFirstProgram {
 
 
     Square s = new Square(5);
-    System.out.println("Площадь квадрата sо sтороной " + s.l + " = " + area(s));
+    System.out.println("Площадь квадрата sо sтороной " + s.l + " = " + s.area());
 
     Rectangle r = new Rectangle(4,6);
-       System.out.println("Площадь прaмоугольника sо sторонами " + r.a + " и " + r.b + " = " + area(r));
+       System.out.println("Площадь прaмоугольника sо sторонами " + r.a + " и " + r.b + " = " + r.area());
 
   }
   public static void hello(String somebody){
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area (Square s){
-    return s.l * s.l;
-  }
 
-  public static double area (Rectangle r) {
-    return r.a * r.b;
-  }
 
   public static double area (double l) {
     return l * l;
