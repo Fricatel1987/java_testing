73102309b9793498eb00533edbfb83fa4be35cba
Fricatel1987 <Fricatel1987@users.noreply.github.com>
2020-07-27 20:03:35 -0400

    Group Deletion Assured

--- addressbook-web-tests/src/test/java/ru/stqa/testing/addressbook/Tests/GroupRemovingTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/testing/addressbook/Tests/GroupRemovingTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.testing.addressbook.Tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.testing.addressbook.model.GroupData;
 
 public class GroupRemovingTests extends TestBase {
 
   @Test
   public void testGroupRemovingTests() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
