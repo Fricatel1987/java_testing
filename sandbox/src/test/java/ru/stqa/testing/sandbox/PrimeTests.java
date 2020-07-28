939e13a0f1592f5323b5a3f5e59b81b375a19f5b
Fricatel1987 <Fricatel1987@users.noreply.github.com>
2020-07-28 15:28:05 -0400

    cycles

---
+++ sandbox/src/test/java/ru/stqa/testing/sandbox/PrimeTests.java
@@ -0,0 +1,27 @@
+package ru.stqa.testing.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimeTests {
+  @Test
+  public void testPrimesFast (){
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+
+  @Test
+  public void testNonPrimes (){
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+  }
+
+  @Test
+  public void testPrimesWhile (){
+    Assert.assertTrue(Primes.isPrimeWhile(Integer.MAX_VALUE));
+  }
+
+  @Test(enabled = false)
+  public void testPrimesLong (){
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+  }
+}
