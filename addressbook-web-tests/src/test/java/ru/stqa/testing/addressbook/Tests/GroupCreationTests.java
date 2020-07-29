05d2437b8f0cba85298fab192ad33a05e04d5fda
Fricatel1987 <Fricatel1987@users.noreply.github.com>
2020-07-29 18:09:06 -0400

    compare list tests

--- addressbook-web-tests/src/test/java/ru/stqa/testing/addressbook/Tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/testing/addressbook/Tests/GroupCreationTests.java
@@ -4,14 +4,16 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.testing.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreationTests() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().createGroup(new GroupData("Test1", "Test2", "Test3"));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before +1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() +1);
   }
 }
